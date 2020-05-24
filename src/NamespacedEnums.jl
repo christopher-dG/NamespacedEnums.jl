453c6890c6b129553601867f73bb456307741e98
Chris de Graaf <me@cdg.dev>
2020-05-24 15:07:39 -0500

    Initial version

--- src/NamespacedEnums.jl
+++ src/NamespacedEnums.jl
@@ -1,5 +1,47 @@
 module NamespacedEnums
 
-# Write your package code here.
+export @nsenum
+
+"""
+    @nsenum [T=T] EnumName[::BaseType] value1[=x] value2[=y]
+
+Create a namespaced enum.
+This enum works similarly a regular `@enum`, but its values are hidden behind a module.
+To access the values, use `EnumName.value1`.
+To access the enum itself, use `EnumName.T`.
+
+```julia-repl
+julia> using NamespacedEnums
+
+julia> @nsenum Fruit apple=1 orange=2 kiwi=3;
+
+julia> f(x::Fruit.T) = "I'm a Fruit with value: \$(Int(x))";
+
+julia> f(apple)
+ERROR: UndefVarError: apple not defined
+
+julia> f(Fruit.apple)
+"I'm a Fruit with value: 1"
+```
+"""
+macro nsenum(exs...)
+    return esc(nsenum(exs...))
+end
+
+function nsenum(exs...)
+    T, name_idx = exs[1] isa Expr && exs[1].head === :(=) ? (exs[1].args[2], 2) : (:T, 1)
+    name = exs[name_idx]
+    if exs[name_idx] isa Expr
+        T = Expr(:(::), T, exs[name_idx].args[2])
+        name = name.args[1]
+    end
+    vals = exs[name_idx+1:end]
+    M = gensym(name)
+    return Expr(
+        :block,
+        Expr(:toplevel, :(module $M @enum $T $(vals...) end)),
+        :(const $name = $M),
+    )
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,27 @@ using NamespacedEnums
 using Test
 
 @testset "NamespacedEnums.jl" begin
-    # Write your tests here.
+    @eval begin
+        @nsenum A a b c
+        @test A isa Module
+        @test :T in names(A; all=true)
+        @test A.T <: Enum
+        for v in (:a, :b, :c)
+            @test getfield(A, v) isa A.T
+        end
+
+        @nsenum T=Name B a=1 b=2 c=3
+        @test !(:T in names(B; all=true))
+        @test :Name in names(B; all=true)
+        @test B.Name <: Enum
+        @test Int(B.a) == 1
+
+        @nsenum C::UInt8 begin
+            a
+            b
+            c
+        end
+        @test C.T <: Enum{UInt8}
+        @test :a in names(C; all=true)
+    end
 end
